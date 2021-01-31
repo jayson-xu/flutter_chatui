e804f045c7df2e802ceeb2ecd4c53c55f2d67ca5
jayson-xu <jayson7710@gmail.com>
2021-01-31 15:33:38 +0800

    定制消息显示位置及风格

--- lib/pages/chat_detail_page.dart
+++ lib/pages/chat_detail_page.dart
@@ -91,7 +91,24 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
               return Container(
                 padding:
                     EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
-                child: Text(messages[index].messageContent),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.grey.shade200
+                          : Colors.blue[200]),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
               );
             },
           ),
