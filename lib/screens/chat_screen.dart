2f0f4310a1ace9f57aa331309933e2edec8104f1
jayson-xu <jayson7710@gmail.com>
2021-01-31 13:18:42 +0800

    美化消息文本的展示(字体、背景、间距)

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -12,8 +12,59 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  _buildMessage(Message message, bool isMe) {
+    return Container(
+      // 自已发送的消息靠右显示，接收到的消息靠左显示
+      margin: isMe
+          ? EdgeInsets.only(top: 8, bottom: 8, left: 80)
+          : EdgeInsets.only(top: 8, bottom: 8, right: 80),
+      // 每句对话文本增加内边距，不然挤一块了
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      decoration: BoxDecoration(
+        // 容器的颜色，也就是背景。注意：有装饰器的时候颜色不能直接设置在Container上
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        // 圆角效果
+        borderRadius: isMe
+            ? BorderRadius.only(
+                topLeft: Radius.circular(15),
+                bottomLeft: Radius.circular(15),
+              )
+            : BorderRadius.only(
+                topRight: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Message> mockMessages = List<Message>();
+    for (int i = 0; i < 1; i++) {
+      mockMessages.addAll(messages);
+    }
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
@@ -52,11 +103,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   topRight: Radius.circular(30),
                 ),
                 child: ListView.builder(
-                  padding: EdgeInsets.only(top:15),
-                  itemCount: messages.length,
+                  reverse: true, // 因为示例消息是越近的消息排得越前，所以这里逆序
+                  padding: EdgeInsets.only(top: 15),
+                  itemCount: mockMessages.length,
                   itemBuilder: (BuildContext context, int index) {
-                    final Message message = messages[index];
-                    return Text(message.text);
+                    final Message message = mockMessages[index];
+                    final isMe = message.sender.id == currentUser.id;
+                    return _buildMessage(message, isMe);
                   },
                 ),
               ),
